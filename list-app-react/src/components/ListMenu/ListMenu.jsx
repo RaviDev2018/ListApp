e67499864b0fe8bda259aa72e672ff4d6b9ec6db
RaviDev2018 <thiara_138@hotmail.com>
2019-04-27 14:45:46 +0100

    Updating side menu styling.

--- list-app-react/src/components/ListMenu/ListMenu.jsx
+++ list-app-react/src/components/ListMenu/ListMenu.jsx
@@ -3,14 +3,19 @@ import React from 'react';
 import ListMenuItem from './ListMenuItem/ListMenuItem';
 
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const ListMenu = (props) => (
-    <Nav defaultActiveKey="/newList" className="flex-column">
-        <Nav.Link href={'/newList'}>Add new list</Nav.Link>
-        {props.lists.map(list => (
-            <ListMenuItem key={list.id} listName={list.name} />
-        ))}
-    </Nav>
+    <Navbar bg="dark" variant="dark">
+        <Nav defaultActiveKey="/newList" className="flex-column" variant="pills" style={{width: "100%"}}>
+            <Nav.Item>
+                <Nav.Link href={'/newList'}>Add new list</Nav.Link>
+            </Nav.Item>
+            {props.lists.map(list => (
+                <ListMenuItem key={list.id} listName={list.name} />
+            ))}
+        </Nav>
+    </Navbar>
 );
 
 export default ListMenu;
--- list-app-react/src/components/ListMenu/ListMenuItem/ListMenuItem.jsx
+++ list-app-react/src/components/ListMenu/ListMenuItem/ListMenuItem.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 
 const ListMenuItem = (props) => (
-    <Nav.Link href={'/List/' + props.listName}>{props.listName}</Nav.Link>
+    <Nav.Item>
+        <Nav.Link href={'/List/' + props.listName}>{props.listName}</Nav.Link>
+    </Nav.Item>
 );
 
 export default ListMenuItem;
